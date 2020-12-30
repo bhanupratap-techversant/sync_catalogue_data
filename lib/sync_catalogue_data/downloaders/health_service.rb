0ba12634a4069473000741c64c3027b706324385
Bhanu Pratap <bhanupratap@techversantinfo.com>
2020-12-30 13:36:09 +0530

    Minor fix

--- lib/sync_catalogue_data/downloaders/health_service.rb
+++ lib/sync_catalogue_data/downloaders/health_service.rb
@@ -6,8 +6,8 @@ module SyncCatalogueData
       HEALTH_SERVICES_DATA_URL = "https://catalogue.data.gov.bc.ca/dataset/a16d7095-9577-4c81-a800-33763ed75c8e/resource/b8fea73b-cb28-43aa-ab22-ce52e80f9941/download/bchealthservicesbygenderandhealthservicedeliveryarea.csv".freeze
 
       def call
-        file_object = open(file_path_for("population_statistics"), 'wb') do |file|
-          file << URI.open(POPULATION_STATISTICS_DATA_URL).read
+        file_object = open(file_path_for("health_services"), 'wb') do |file|
+          file << URI.open(HEALTH_SERVICES_DATA_URL).read
         end
         file_object.path
       end
